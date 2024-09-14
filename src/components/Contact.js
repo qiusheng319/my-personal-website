bafc5b77bf7a165856c4cf38ba9839264c2116d3
CHOSEN <6567091+javachosen@user.noreply.gitee.com>
2024-09-14 12:33:27 +0800

    f

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+            <p>Email: your.email@example.com</p>
+        </div>
+    );
+};
+
+export default Contact;
